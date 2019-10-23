4ecc3af5760628b55a9e67d012ae4b475834d14e
Patrick Allman <pallman89@icloud.com>
2019-10-23 10:09:01 -0700

    New Update to fix arrows and design changes

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function search_photo() { 
+    let input = document.getElementById('search').value 
+    input=input.toLowerCase(); 
+    let x = document.getElementsByClassName('row'); 
+      
+    for (i = 0; i < x.length; i++) {  
+        if (!x[i].innerHTML.toLowerCase().includes(input)) { 
+            x[i].style.display="none"; 
+        } 
+        else { 
+            x[i].style.display="jpg";                  
+        } 
+    } 
+} 
